dc88cff8eeeb6ede449c7b42f682fe3d77e2ff02
Manigandan <54367953+iamurmani@users.noreply.github.com>
2024-01-18 00:42:36 +0530

    image url changes

--- scripts/data.js
+++ scripts/data.js
@@ -3,70 +3,70 @@ var songs = [
         id:1,
         name:"Naa Ready",
         artist:"Anirudg Ravichander",
-        imagePath:"/images/naready.jpg",
+        imagePath:"images/naready.jpg",
         time:"4:08"
     },
     {
         id:2,
         name:"Kaavaalaa",
         artist:"Anirudg Ravichander",
-        imagePath:"/images/kavala.jpeg",
+        imagePath:"images/kavala.jpeg",
         time:"4:56"
     },
     {
         id:3,
         name:"Jimikki Ponnu",
         artist:"Anirudg Ravichander",
-        imagePath:"/images/jimikki.jpg",
+        imagePath:"images/jimikki.jpg",
         time:"5:02"
     },
     {
         id:4,
         name:"Chilla Chilla",
         artist:"Gibran",
-        imagePath:"/images/chilla.jpg",
+        imagePath:"images/chilla.jpg",
         time:"4:38"
     },
     {
         id:5,
         name:"Ranjithame",
         artist:"Anirudh Ravichander",
-        imagePath:"/images/ranjithame.jpg",
+        imagePath:"images/ranjithame.jpg",
         time:"5:04"
     },
     {
         id:6,
         name:"Raawadi",
         artist:"A.R. Rahman",
-        imagePath:"/images/rawadi.jpeg",
+        imagePath:"images/rawadi.jpeg",
         time:"4:20"
     },
     {
         id:7,
         name:"Maamadurai",
         artist:"Santhosh Narayanan",
-        imagePath:"/images/maamudurai.jpg",
+        imagePath:"images/maamudurai.jpg",
         time:"4:55"
     },
     {
         id:8,
         name:"Jorthaale",
         artist:"Asal Kolaar",
-        imagePath:"/images/jorthale.jpg",
+        imagePath:"images/jorthale.jpg",
         time:"4:29"
     },
     {
         id:9,
         name:"Kasethan Kadavulada",
         artist:"Gibran",
-        imagePath:"/images/kasu.jpg",
+        imagePath:"images/kasu.jpg",
         time:"4:30"
     },
     {
         id:10,
         name:"Namma satham",
         artist:"A.R. Rahaman",
-        imagePath:"/images/nammasatham.jpg",
+        imagePath:"images/nammasatham.jpg",
         time:"4:55"
     }
 
