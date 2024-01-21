d8fec2954e14d20043e7fbe3d98ca227c851e1b5
Manigandan <54367953+iamurmani@users.noreply.github.com>
2024-01-21 15:59:31 +0530

    add music controls

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,15 @@
 let songContainer = document.querySelector('#song-container');
 let playerContainer = document.querySelector('#player-container');
+let title = document.querySelector('.title');
+let welcome = document.querySelector('.welcome');
+let playingSong;
+let progressBar = document.querySelector(".progress-bar");
+let pause;
+let play;
+let sound;
+let mute;
+var currentTimeDisplay;
+var durationDisplay;
 function getAllSongItemsList() {
     let songItemsList = '';
     for (let i = 0; i < songs.length; i++) {
@@ -12,7 +22,7 @@ function getAllSongItemsList() {
 }
 
 function getSongItem(song, fromPlayer) {
-    let songItem = `<div class="song-item" onclick="${!fromPlayer ? `openPlayer(${song.id})` : `loadSong(${song.id})`}">
+    let songItem = `<div class="song-item" id="song-item-${song.id}" onclick="${!fromPlayer ? `openPlayer(${song.id})` : `loadSong(${song.id})`}">
     <div class="song-details">
         <img src="${song.imagePath}" alt="">
         <div class="song-detail">
@@ -32,6 +42,8 @@ getAllSongItemsList();
 function openPlayer(id) {
     songContainer.classList.add('hide');
     playerContainer.classList.remove('hide');
+    title.classList.add('hide');
+    welcome.classList.add('hide');
     let selectedSong = songs.find(x => x.id === id);
     let playingSong = getPlayingSongPlayer(selectedSong);
     let player = document.createElement('div');
@@ -46,7 +58,12 @@ function openPlayer(id) {
     let listItems = document.createElement('div');
     listItems.innerHTML = songLists;
     listItems.classList.add('song-items');
-    playerContainer.appendChild(listItems)
+    playerContainer.appendChild(listItems);
+    playSelectedSong(`song-${id}`);
+    let items = document.querySelectorAll(`#song-item-${id}`);
+    items.forEach(ele => {
+        ele.classList.add('green-border');
+    })
 }
 
 function getPlayingSongPlayer(song) {
@@ -59,30 +76,47 @@ function getPlayingSongPlayer(song) {
 </div>
 <div class="song-playing-details">
     <div class="palying-song">
-        <p class="sound"><i class="fas fa-volume-up"></i></p>
+        <p class="sound" onClick="soundOrMute()"><i class="fas fa-volume-up"></i></p>
+        <p class="mute" onClick="soundOrMute()"><i class="fas fa-volume-mute"></i></p>
         <p>${song.name}</p>
-        <p class="heart"><i class="far fa-heart"></i></p>
+        <p  onClick="likeSong()"><i class="far fa-heart heart"></i></p>
     </div>
     <p class="song-artist">${song.artist}</p>
 </div>
 <div class="player-conrols">
-    <div><i class="fas fa-step-backward"></i></div>
+    <div><i class="fas fa-step-backward" onClick="previous(${song.id - 1})"></i></div>
     <div class="controls">
-        <i class="fas fa-backward"></i>
-        <i class="fas fa-pause"></i>
-        <i class="fas fa-forward"></i>
+        <i class="fas fa-backward" onClick="rewind()"></i>
+        <i class="fas fa-pause" id="pause" onClick="pauseOrResume()"></i>
+        <i class="fas fa-play" id="play" onClick="pauseOrResume()"></i>
+        <i class="fas fa-forward" onClick="forward()"></i>
     </div>
-    <div><i class="fas fa-step-forward"></i></div>
-</div>`
+    <div><i class="fas fa-step-forward" onClick="next(${song.id + 1})"></i></div>
+</div>
+<audio id="song-${song.id}" src="${song.songPath}"></audio>
+<div class="bar">
+        <p class="current-time">00:00</p>
+        <div class="progress-bar-container">
+            <div class="progress-bar"></div>
+        </div>
+        <p class="audio-duration">00:00</p>
+</div>
+`
 }
 
 function closePlayer(){
     playerContainer.classList.add('hide');
     playerContainer.innerHTML = '';
     songContainer.classList.remove('hide');
+    title.classList.remove('hide');
+    welcome.classList.remove('hide');
 }
 
 function loadSong(id) {
+    let borders = document.querySelectorAll('.green-border');
+    borders.forEach(ele => {
+        ele.classList.remove('green-border');
+    })
     let playerEle = document.querySelector('.player');
     let selectedSong = songs.find(x => x.id === id);
     let playingSong = getPlayingSongPlayer(selectedSong);
@@ -90,4 +124,116 @@ function loadSong(id) {
     player.innerHTML = playingSong;
     player.classList.add('player');
     playerEle.replaceWith(player);
+    playSelectedSong(`song-${id}`);
+    let items = document.querySelectorAll(`#song-item-${id}`);
+    items.forEach(ele => {
+        ele.classList.add('green-border');
+    })
 }
+
+function playSelectedSong(id) {
+    playingSong = document.getElementById(id);
+    playingSong.play();
+    playingSong.addEventListener("timeupdate", updateProgressBar);
+    progressBar = document.querySelector(".progress-bar");
+    pause = document.getElementById('pause');
+    play = document.getElementById('play');
+    sound = document.querySelector('.sound');
+    mute = document.querySelector('.mute');
+    play.classList.add('hide');
+    mute.classList.add('hide');
+    currentTimeDisplay = document.querySelector(".current-time");
+    durationDisplay = document.querySelector(".audio-duration");
+    updateProgressBar();
+}
+
+function updateProgressBar(){
+    var percentage = (playingSong.currentTime / playingSong.duration) * 100;
+        progressBar.style.width = percentage + "%";
+
+
+        var currentTimeMinutes = Math.floor(playingSong.currentTime / 60);
+        var currentTimeSeconds = Math.floor(playingSong.currentTime % 60);
+        currentTimeDisplay.innerText = currentTimeMinutes + ":" + (currentTimeSeconds < 10 ? "0" : "") + currentTimeSeconds;
+
+        var durationMinutes = Math.floor(playingSong.duration / 60);
+        var durationSeconds = Math.floor(playingSong.duration % 60);
+        durationDisplay.innerText = durationMinutes + ":" + (durationSeconds < 10 ? "0" : "") + durationSeconds;
+
+}
+
+function pauseOrResume (){
+    if (playingSong.paused) {
+        playAudio();
+        play.classList.add('hide');
+        pause.classList.remove('hide');
+    } else {
+        pauseAudio();
+        pause.classList.add('hide');
+        play.classList.remove('hide');
+    }
+}
+
+function playAudio() {
+    playingSong.play();
+    updateProgressBar();
+}
+
+function pauseAudio() {
+    playingSong.pause();
+}
+
+function forward(){
+    playingSong.currentTime = playingSong.currentTime + 5
+    updateProgressBar();
+  }
+
+  function rewind(){
+    playingSong.currentTime = playingSong.currentTime - 5
+    updateProgressBar();
+  }
+
+function previous(id){
+    if(id > 0 && id <= songs.length) {
+        loadSong(id);
+    }
+}
+
+function next(id){
+    if(id > 0 && id <= songs.length) {
+        loadSong(id);
+    }
+}
+
+function getMinutes(t){
+    var min = parseInt(parseInt(t)/60);
+    var sec = parseInt(t%60);
+    if (sec < 10) {
+      sec = "0"+sec
+    }
+    if (min < 10) {
+      min = "0"+min
+    }
+    return min+":"+sec
+  }
+
+  function soundOrMute(){
+    if (playingSong.muted) {
+        playingSong.muted = false;
+        mute.classList.add('hide');
+        sound.classList.remove('hide');
+    } else {
+        playingSong.muted = true;
+        sound.classList.add('hide');
+        mute.classList.remove('hide');
+    }   
+  }
+
+  function likeSong(){
+    let likeEle = document.querySelector('.heart');
+    if(likeEle.classList.contains('red')){
+        likeEle.classList.remove('red');
+    } else {
+        likeEle.classList.add('red');
+    }
+  }
--- scripts/data.js
+++ scripts/data.js
@@ -4,70 +4,80 @@ var songs = [
         name:"Naa Ready",
         artist:"Anirudg Ravichander",
         imagePath:"images/naready.jpg",
-        time:"4:08"
+        time:"4:08",
+        songPath:"music/Naaready.mp3"
     },
     {
         id:2,
         name:"Kaavaalaa",
         artist:"Anirudg Ravichander",
         imagePath:"images/kavala.jpeg",
-        time:"4:56"
+        time:"3:10",
+        songPath:"music/Kaavaalaa.mp3"
     },
     {
         id:3,
         name:"Jimikki Ponnu",
         artist:"Anirudg Ravichander",
         imagePath:"images/jimikki.jpg",
-        time:"5:02"
+        time:"3:44",
+        songPath:"music/Jimikkiponnu.mp3"
     },
     {
         id:4,
         name:"Chilla Chilla",
         artist:"Gibran",
         imagePath:"images/chilla.jpg",
-        time:"4:38"
+        time:"3:38",
+        songPath:"music/ChillaChilla.mp3"
     },
     {
         id:5,
         name:"Ranjithame",
         artist:"Anirudh Ravichander",
         imagePath:"images/ranjithame.jpg",
-        time:"5:04"
+        time:"4:47",
+        songPath:"music/Ranjithame.mp3"
     },
     {
         id:6,
         name:"Raawadi",
         artist:"A.R. Rahman",
         imagePath:"images/rawadi.jpeg",
-        time:"4:20"
+        time:"3:11",
+        songPath:"music/Rawadi.mp3"
     },
     {
         id:7,
         name:"Maamadurai",
         artist:"Santhosh Narayanan",
         imagePath:"images/maamudurai.jpg",
-        time:"4:55"
+        time:"2:48",
+        songPath:"music/Mamudarai.mp3"
     },
     {
         id:8,
         name:"Jorthaale",
         artist:"Asal Kolaar",
         imagePath:"images/jorthale.jpg",
-        time:"4:29"
+        time:"1:50",
+        songPath:"music/Jorthale.mp3"
     },
     {
         id:9,
         name:"Kasethan Kadavulada",
         artist:"Gibran",
         imagePath:"images/kasu.jpg",
-        time:"4:30"
+        time:"3:08",
+        songPath:"music/Kasethan.mp3"
     },
     {
         id:10,
         name:"Namma satham",
         artist:"A.R. Rahaman",
         imagePath:"images/nammasatham.jpg",
-        time:"4:55"
+        time:"3:35",
+        songPath:"music/Nammasatham.mp3"
     }
 
 ]
